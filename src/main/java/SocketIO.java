74e0ca4cfd567dc3c89504985effc1189201cf0a
zhaokun3 <zhaokun3@gomefinance.com.cn>
2020-08-19 09:46:16 +0800

    多线程访问Nio

--- src/main/java/SocketIO.java
+++ src/main/java/SocketIO.java
@@ -15,18 +15,30 @@ public class SocketIO {
             ServerSocket serverSocket=new ServerSocket(9090);
             System.out.println("Set1:new ServerSocket(9090)");
             while (true){
-                Socket client = serverSocket.accept();//堵塞
+                final Socket client = serverSocket.accept();//堵塞
                 System.out.println("Set2:"+client.getPort());
-                InputStream inputStream = client.getInputStream();
-                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
-                String read = bufferedReader.readLine();//堵塞
-                System.out.println(read);
-                while (true){
-
-                }
+                /* //多线程访问
+                new Thread(){
+                    @Override
+                    public void run() {
+                        read(client);
+                    }
+                }.start();*/
+                read(client);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public static void read(Socket client){
+        try {
+            InputStream inputStream = client.getInputStream();
+            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+            String read = bufferedReader.readLine();//堵塞
+            System.out.println(read);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
